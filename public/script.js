cf964233744d09cd4359124a6cfd55d46a32bb62
Kevin Gerken <kevin@kevingerken.com>
2018-04-26 19:40:19 -0500

    add accessiblity to links, buttons, inputs, textareas and star ratings

--- public/script.js
+++ public/script.js
@@ -1,24 +1,8 @@
-const stars = document.querySelectorAll(`.rating-stars`),
-      numInput = document.querySelector(`.num-input`),
-      deleteButton = document.querySelector(`.delete-button`);
-
-for(let star of stars) {
-  star.addEventListener(`click`, () => {
-    let rating = star.classList[1].slice(-1);
-    numInput.value = rating;
-    for(let star of stars) {
-      star.classList.remove(`clicked`);
-    }
-    for(let i = rating; i > 0; i--) {
-      document.querySelector(`.star${i}`).classList.add(`clicked`);  
-    }
-  });
-}
-
-function formSubmit() {
-  document.querySelector(`.delete-form`).submit();
-}
+const deleteButton = document.querySelector(`.delete-button`);
 
 if(deleteButton) {
-  deleteButton.addEventListener(`click`, formSubmit)
+  deleteButton.addEventListener(`click`, () => {
+    document.querySelector(`.delete-form`).submit();
+  })
 }
+
