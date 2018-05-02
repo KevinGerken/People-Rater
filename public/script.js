a7149a9de098ef81799de1c01628b82f5848af78
Kevin Gerken <kevin@kevingerken.com>
2018-05-02 15:49:43 -0500

    logged in as link now goes to user edit, log in link changes header to log in form

--- public/script.js
+++ public/script.js
@@ -6,3 +6,19 @@ if(deleteButton) {
   })
 }
 
+const loginBtn = document.querySelector(`.login-btn`);
+const headerNav = document.querySelector(`.header-nav`); 
+const cancel = document.querySelector(`.cancel`);
+const headerLogin = document.querySelector(`.header-login`);                               
+
+loginBtn.addEventListener(`click`, (event) => {
+  console.log(`huh`);
+  event.preventDefault();
+  headerNav.classList.add(`hidden`);
+  headerLogin.classList.remove(`hidden`);
+});
+
+cancel.addEventListener(`click`, (evt) => {
+  headerNav.classList.remove(`hidden`);
+  headerLogin.classList.add(`hidden`);
+});
